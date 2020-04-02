ed5ac3046afa9a27fd6738b0adb446363a31d517
Tian Liu <8xiaolong8@gmail.com>
2020-04-01 23:23:48 -0400

    Basic UI implemented
    Added basic UI, background images, some sound effects, and bgm.

--- Script.js
+++ Script.js
@@ -2,6 +2,8 @@
 document.getElementById("menu").addEventListener("keypress", function () {
     if (event.keyCode == 32) {
         $("#menu").remove();
+        $('audio')[0].play();
+        $('audio').prop("volume", 0.3);
     }
 });
 
@@ -9,16 +11,19 @@ document.getElementById("menu").addEventListener("keypress", function () {
 const VH = function () {
     return $(`
         <div class="house">
-        <p>You walk towards the house and knock.  There is no response. You push the door open and to your surprise, it is unlocked. You enter the lobby of the house. </p>
-        <button id="kitchen">Kitchen</button>
-        <button id="bedroom">Bedroom</button>
-        <button id="bathroom">Bathroom</button>
+            <div id="buttons">
+                <button class="option" id="kitchen">Kitchen</button>
+                <button class="option" id="bedroom">Bedroom</button>
+                <button class="option" id="bathroom">Bathroom</button>
+            </div>
+            <p id="paragraphs">You walk towards the house and knock. There is no response. You push the door open and to your surprise, it is unlocked. You enter the lobby of the house.</p>
+            <audio controls autoplay class="music"><source src="./Assets/FootstepSE.mp3" type='audio/mp3'></audio>
         </div>
     `);
 }
 document.getElementById("visitHouse").addEventListener("click", function () {
     event.preventDefault();
-    event.target.parentNode.remove();
+    event.target.parentNode.parentNode.remove();
     $('#root').append(VH);
     kit();
 })
@@ -27,9 +32,12 @@ document.getElementById("visitHouse").addEventListener("click", function () {
 const CTA = function () {
     return $(`
         <div class="tripleA">
-        <p> Looks like there is no service </p>
-        <button id="visitHouse">Visit the house</button>
-        <button id="HitchRide">Hitch a ride</button>
+            <div id="buttons">
+                <button class="option" id="visitHouse">Visit the house</button>
+                <button class="option" id="HitchRide">Hitch a ride</button>
+            </div>
+            <p id="paragraphs">Looks like there is no service.</p>
+            <audio controls autoplay loop class="music" id="call"><source src="./Assets/CallSE.mp3" type='audio/mp3'></audio>
         </div>
     `);
 }
@@ -42,7 +50,7 @@ document.getElementById("callTripleA").addEventListener("click", function () {
     //visit house 
     document.getElementById("visitHouse").addEventListener("click", function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(VH);
         kit();
     })
@@ -50,12 +58,12 @@ document.getElementById("callTripleA").addEventListener("click", function () {
     // Hitch a Ride
     document.getElementById("HitchRide").addEventListener("click", function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(HR);
 
         document.getElementById("visitHouse").addEventListener("click", function () {
             event.preventDefault();
-            event.target.parentNode.remove();
+            event.target.parentNode.parentNode.remove();
             $('#root').append(VH);
             kit()
         })
@@ -67,10 +75,12 @@ document.getElementById("callTripleA").addEventListener("click", function () {
 const HR = function () {
     return $(`
         <div class="ride">
-        <p> You wait for a while but no one passes by. What do you do?  </p>
-        <button id="visitHouse">Visit the house</button>
-        <button id="callTripleA">Call Triple A</button>
-        <button id="wait">Keep waiting</button>
+            <div id="buttons">
+                <button class="option" id="visitHouse">Visit the house</button>
+                <button class="option" id="callTripleA">Call Triple A</button>
+                <button class="option" id="wait">Keep waiting</button>
+            </div>
+        <p id="paragraphs">You wait for a while but no one passes by. What do you do?</p>
         </div>
     `);
 }
@@ -84,19 +94,19 @@ document.getElementById("HitchRide").addEventListener("click", function () {
     //visit house 
     document.getElementById("visitHouse").addEventListener("click", function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(VH);
         kit()
     })
 
     document.getElementById("callTripleA").addEventListener("click", function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(CTA);
 
         document.getElementById("visitHouse").addEventListener("click", function () {
             event.preventDefault();
-            event.target.parentNode.remove();
+            event.target.parentNode.parentNode.remove();
             $('#root').append(VH);
             kit();
         })
@@ -106,10 +116,14 @@ document.getElementById("HitchRide").addEventListener("click", function () {
 
 //keep waiting 
 const kw = function () {
-    return $(`
-        <p> You wait some more, but no one is here. </p>  
-        <button id="visitHouse">Visit the house</button>
-        <button id="wait">Keep waiting</button>
+    return $(` 
+        <div class="ride">
+            <div id="buttons">
+                <button class="option" id="visitHouse">Visit the house</button>
+                <button class="option" id="wait">Keep waiting</button>
+            </div>
+            <p id="paragraphs"> You wait some more, but no one is here. </p> 
+        </div>
     `);
 }
 const kw2 = function () {
@@ -123,12 +137,12 @@ const kw2 = function () {
 function keepwait() {
     $("#wait").click(function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(kw);
 
         document.getElementById("visitHouse").addEventListener("click", function () {
             event.preventDefault();
-            event.target.remove();
+            event.target.parentNode.parentNode.remove();
             $('#root').append(VH);
             kit();
         })
@@ -138,12 +152,12 @@ function keepwait() {
 function keepwait2() {
     $("#wait").click(function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('body').append(kw);
 
         document.getElementById("visitHouse").addEventListener("click", function () {
             event.preventDefault();
-            event.target.parentNode.remove();
+            event.target.parentNode.parentNode.remove();
             $('body').append(VH);
             kit();
         })
@@ -154,32 +168,40 @@ function keepwait2() {
 //kitchen
 const kt = function () {
     return $(`
-        <p> You enter the kitchen. There’s a large fridge to your left, and a kitchen counter next to it. You hear a peculiar sound coming from the counter.  What do you do?</p>  
-        <button id="investigate"> Investigate the noise </button>
-        <button id="hide"> Hide </button>
+        <div class="kitchen">
+            <div id="buttons">
+                <button class="option" id="investigate"> Investigate the noise </button>
+                <button class="option" id="hide"> Hide </button>
+            </div>
+            <p id="paragraphs"> You enter the kitchen. There’s a large fridge to your left, and a kitchen counter next to it. You hear a peculiar sound coming from the counter.  What do you do?</p>  
+            <audio controls autoplay loop class="music"><source src="./Assets/ChopSE.mp3" type='audio/mp3'></audio>
+        </div>    
     `);
 }
 
 const hideInKitchen = function () {
     return $(`
-        <p> You find a cleaning closet to your right and dash inside. You feel something by your feet and pick it up. It’s a flashlight. After some time, the noises stop, you leave the kitchen and go back to the lobby. </p>  
-        <button id="back"> Back to Lobby </button>
+        <div class="hide">
+            <div id="buttons"><button class="option" id="back"> Back to Lobby </button></div>
+            <p id="paragraphs">You find a cleaning closet to your right and dash inside. You feel something by your feet and pick it up. It’s a flashlight. After some time, the noises stop, you leave the kitchen and go back to the lobby. </p>  
+            <audio controls autoplay class="music"><source src="./Assets/DoorSE.mp3" type='audio/mp3'></audio>
+        </div>
     `);
 }
 
 function kit() {
     $("#kitchen").click(function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('#root').append(kt);
-        hide()
+        hide();
     })
 }
 
 function hide() {
     $("#hide").click(function () {
         event.preventDefault();
-        event.target.parentNode.remove();
+        event.target.parentNode.parentNode.remove();
         $('body').append(hideInKitchen);
     })
 }
